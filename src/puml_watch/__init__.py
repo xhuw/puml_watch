10e6e23f1a82f999ce34dbb550b9c91c02ac458e
Huw Percival <huwpercival@xmos.com>
2024-09-06 14:54:12 +0100

    Add option to run puml once

--- src/puml_watch/__init__.py
+++ src/puml_watch/__init__.py
@@ -5,6 +5,7 @@ from subprocess import run
 import puml_watch_plantuml as puml
 import argparse
 
+
 class Log:
     def __init__(self):
         self.quiet = False
@@ -13,13 +14,22 @@ class Log:
         if not self.quiet:
             print(*args, **kwargs)
 
+
 L = Log()
+
 
 def parse_args(argv):
     parser = argparse.ArgumentParser("puml_watch")
     parser.add_argument("FILES", nargs="+")
     parser.add_argument("--out-dir", "-o", default=".")
-    parser.add_argument("--quiet", "-q", action="store_true", help="remove output except from plantuml.")
+    parser.add_argument(
+        "--quiet", "-q", action="store_true", help="remove output except from plantuml."
+    )
+    parser.add_argument(
+        "--once",
+        action="store_true",
+        help="Run plantuml once on all the provided files",
+    )
     return parser.parse_args(argv)
 
 
@@ -29,6 +39,11 @@ def do_puml(puml_jar, path, out_dir):
     run(["java", "-jar", puml_jar, path, "-output", out_dir])
 
 
+def puml_once(puml_jar, paths, out_dir):
+    for file in paths:
+        do_puml(puml_jar, file, out_dir)
+
+
 def puml_watch(args):
     out_dir = Path(args.out_dir)
     if not out_dir.exists():
@@ -36,10 +51,12 @@ def puml_watch(args):
         return 1
     try:
         with puml.plantuml_path() as plantuml:
-            for changes in watch(*args.FILES):
-                for change, path in changes:
-                    if change != Change.deleted:
-                        do_puml(plantuml, path, out_dir)
+            puml_once(plantuml, args.FILES, out_dir)
+            if not args.once:
+                for changes in watch(*args.FILES):
+                    for change, path in changes:
+                        if change != Change.deleted:
+                            do_puml(plantuml, path, out_dir)
     except KeyboardInterrupt:
         pass
 
